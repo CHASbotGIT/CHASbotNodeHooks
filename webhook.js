c34602e6eb652413d71517a68b10525198918b50
CHASbotGIT <32416795+CHASbotGIT@users.noreply.github.com>
2017-11-17 11:47:30 +0000

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -108,7 +108,7 @@ const URL_API_MARVEL = "https://gateway.marvel.com/v1/public/characters?nameStar
 const URL_GROUP_DOCS = "https://work-" + KEY_ROOT + ".facebook.com/groups/1707079182933694";
 const URL_GROUP_HELP = "https://work-" + KEY_ROOT + ".facebook.com/groups/733856763459096/files/";
 const URL_CHAS_RETAIL = "https://www.chas.org.uk/contact_chas#retail";
-const URL_CHAS_PLAN = "https://s3-eu-west-1.amazonaws.com/chas-assets/downloads/3958+CHAS+Infographic-Plan-download.pdf"
+const URL_CHAS_PLAN = "https://s3-eu-west-1.amazonaws.com/chas-assets/downloads/3958+CHAS+Infographic-Plan-download.pdf";
 const URL_SEARCH_GOOGLE = "https://www.google.com/search?q=";
 const URL_SEARCH_WIKI = "https://en.wikipedia.org/w/index.php?search=";
 const URL_SEARCH_BEEB = "https://www.bbc.co.uk/search?q=";
