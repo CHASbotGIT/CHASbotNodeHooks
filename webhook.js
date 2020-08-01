3df0e4f9d6537a6ac0e6cfeaa0fbb2a573b32ec7
CHASbot <chasbotic@gmail.com>
2020-08-01 18:47:50 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1486,13 +1486,27 @@ function sendTextDirect(eventSend,outbound_text) {
   }); // request
 }
 
+const GOOGLE_PROJECT_ID = "chasbot-c43d7";
+const GOOGLE_CLIENT_EMAIL = "dialogflow-murapr@chasbot-c43d7.iam.gserviceaccount.com";
+const GOOGLE_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCZS1U7kmASrkYp\nvQj0ZHIEdIZGqfrRHhngNIBfvu5Zxska/pFxAwKn1X42XlbIdy1ZtZ1mbZVW97sZ\nRZKfijMZTFy28KyJKmrL4B7bcDrS8eBsuVkOZoGhJ/ZtoAJiiiIOD5MKtqBEvNDI\n4TMY0FSdLbj5X19B992seGgvfXafywBz9uzGaaCY/66ihFKhwg60MmBHmcDwmRxU\nwtmbUHNdyZ9LqWZ6AxjUb4TRMXwjY1BMYyHSq7kqPy9qFerjQhHgDj6nwmhmAftP\njBWDiZN+ZAAi4o9WWTsmypOQ62+l731qcyNST52HAE3geDbSJTUQywyfJBJb36u0\ndaK66jNFAgMBAAECggEAL0yQxHxSO4FdcdR7llkF2z0PD2+0TeDxT/4zHBfgGIBf\nHf6tZsbsg1e7BVCPi3RGYHdK1Ud7vGVc7BvM9YAhay1mMCZLvlkBsd159/CFnf/2\n5OU9xtU3K0AW59Vm1vvTLS5+CuC1y+/MpibiasYhTYxFlmBoloTB980BLKIUP5Jb\njx1NK8W4sI63L9uv5siGD0d/q8Wj3FRrIdWqijiB0jd2ZYgX1ybrpcPTOw+NCHv2\nvckNHn7cEaDVLgsrguUsRcgNxofOS0J9m9BhmsmCgbZmtbpg+ic2jZ7b9C/MaQnH\nWqbK0UrhfPFdAlrmcuwyos4mTEzeWlQsqF2yTvHMAwKBgQDT5izYEYl4Lmr/WEs4\n4YM8IWI2bf67f6USdNlJ6nINr/6h4OxqtKhyHl3utQ1+afWQNR4fYwbGk7bMKdpG\neK6YlClP0Rw2+tfT8Rmc7wcm7/upgWgcH913F6ORXSeeHYh592DbHyl/cw2D0dCN\nUwBDNs26z+LLDpvBp1L27SzFKwKBgQC5MrxPS43H9TTsRw2pW5HApLT/Blw9WoQ/\nK09vrPtJSvRPXsS9PbhZUC7bGdU7vOXExzfOes9/EUvIB9+cdxNFtVihTXD61cgm\nUHbsBctPIDLr4ByMvasslj1j4nRGmZksRZoXIR5sUVMwGUcTodyoFTb6L3rfT9De\nWmgE7USRTwKBgQCUOr8CAvqMVMRKjWRJjQ+1v5CpjLaWSQubZpwd5eAVDRlt+h7H\n4kq3UeDZvFONK+g5Eb9Zq3cw8GH+XgSwKXnRiEuB50D6gOz7+Pu22UrChTwh7kF6\n8xuEVMf6RxUQuY4f69oYTqtD2aMJpHtkcTyMyEiQNx5qd3Sjuj4COBWKcwKBgHiF\nHOjZeT3sQ/GTV7+j2JkuVp8z62j989wL+ljpkGok/hJ80Ll6ZfKxbJynFg4XIsvI\n/XdAkrHVdqSJAysWjpMprbDi7ag+4U6VxJ5aMvGeOaUopkuszq1oZDERWKvtHzIh\nZWKx6dOBpQpDGO8cwbUcgTeVGHgkv/9DhDfYlGufAoGAHGj+cAVHuG/YK+uvqrHF\nNR7YzR7ehdFWJw6I7fX84K3M3UO/ykSAG10P7VY5oCSwSk8UAkjL9XSG3mgvURF/\n1A+Rv0DuA61zcXBrRj1jNVH75yO/8FSeJBdrdXK6oB9jy1u1LeB0Dsal0P6YMDHJ\nuHU+nKghFwgf1Zcd3HWNpPI=\n-----END PRIVATE KEY-----\n";
+const credentials = {
+  client_email: config.GOOGLE_CLIENT_EMAIL,
+  private_key: config.GOOGLE_PRIVATE_KEY,
+};
+const sessionClient = new dialogflow.SessionsClient(
+  {
+    projectId: config.GOOGLE_PROJECT_ID,
+    credentials
+  }
+);
 
+//https://github.com/kamjony/Chatbot-DialogFlowV2-Messenger-NodeJS
 async function sendViaDialogV2(eventSend) {
   //sendTypingOn(sender);
-  const PROJECT_ID = 'chasbot-c43d7'
+  console.log('sendViaDialogV2')
   let sender = eventSend.sender.id;
   let dialogFlowQuery = eventSend.message.text;
-  const sessionClient = new dialogflow.SessionsClient();
+  console.log(dialogFlowQuery);
   try {
     const sessionPath = sessionClient.sessionPath(
       config.PROJECT_ID,
