c5f793b85919bb9a650ef81405817e2a1b2d43e8
CHASbot <chasbotic@gmail.com>
2020-08-08 17:20:58 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -2360,7 +2360,7 @@ function apiLOTR (eventLOTR,lotrWho){
     res.on('end', function() {
       let characterData = JSON.parse(body);
       let characterData_legible = JSON.stringify(characterData);
-      console.log("DEBUG [apiLOTR]> Character JSON: " + characterData_legible);
+      //console.log("DEBUG [apiLOTR]> Character JSON: " + characterData_legible);
       // Correct responses start with "docs" i.e. no status code 200 to help verify
       if (characterData_legible.includes('docs')) {
         let characterDataList = characterData.docs;
