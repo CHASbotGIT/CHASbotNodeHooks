1c908f092293b17043c57d5648af7e9fe653bc77
CHASbot <chasbotic@gmail.com>
2020-08-15 18:23:25 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -2715,7 +2715,7 @@ function apiHERO (eventHero,heroWho){
     res.on('end', function() {
       let heroData = JSON.parse(body);
       console.log("DEBUG [apiHERO]> Got this back: " + heroData);
-      console.log("DEBUG [apiHERO]> Response Code: " + heroData.reponse);
+      console.log("DEBUG [apiHERO]> Response Code: " + heroData.response);
     }); // res.on('end'
   }); // http.get(url
   req.on('error', function(e) { // Catches failures to connect to the API
