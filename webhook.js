d1839454843548c377c144ff5286437a68eed190
CHASbot <chasbotic@gmail.com>
2020-08-21 16:06:45 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -3755,7 +3755,7 @@ function playTopTrumps(eventTT,playTT){ // IN DEV
     //console.log("DEBUG [playTopTrumps]> Stats: " + test_add);
     SENDERS[custom_id][20] = 'category';
     SENDERS[custom_id][16] = SENDERS[custom_id][17]; // new card becomes old
-    SENDERS[custom_id][15].push(SENDERS[custom_id][16]); // old card stored in list
+    SENDERS[custom_id][15].push(SENDERS[custom_id][16],true); // old card stored in list
 
 
     if (trumps_start) { tt_msg = MSG_TOPTRUMPS_INTRO1 + HERO_ARRAY[SENDERS[custom_id][16]][0] + MSG_TOPTRUMPS_INTRO2 };
