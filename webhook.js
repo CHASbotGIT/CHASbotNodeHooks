bc9716d071cc0e1a86dde1dfda2f0821a8a87c10
CHASbot <chasbotic@gmail.com>
2020-08-23 00:39:51 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -3784,14 +3784,14 @@ console.table(SENDERS[custom_id][15]);
     for (playTT_loop = 0; playTT_loop < SENDERS[custom_id][15].length; playTT_loop++) {
       possibleRepeat = SENDERS[custom_id][16];
 
-console.log("^^^^^^^^^^^^^^^^^^^^^^^" + indexFind[playTT_loop])
+console.log("^^^^^^^^^^^^^^^^^^^^^^^ " + indexFind[playTT_loop])
 
       //if (indexFind[playTT_loop].includes(possibleRepeat)) { splceID = playTT_loop }
     }; // for (playTT_loop = 0
 
 console.log(">>>>>>>>>>>>>>>>>>>>>>>>> " + splceID);
 
-    SENDERS[custom_id][20].splice(splceID,1);
+    SENDERS[custom_id][20].splice(1,1);
 
 console.table(SENDERS[custom_id][15]);
 
