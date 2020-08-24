3540043804d5405d3496a7b03685178413acef4c
CHASbot <chasbotic@gmail.com>
2020-08-24 22:45:18 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -3931,3 +3931,47 @@ if (trumps_restart) { tt_msg = "Let's return to " + HERO_ARRAY[SENDERS[custom_id
     console.log("INFO [playTopTrumps]> Response: " + tt_msg);
   };
 }
+
+var pokemonInfo = [];
+var pokePics = [];
+//var pokemon;
+
+async function fetchPokemon(pokemonId) {
+ var response = await fetch('https://pokeapi.co/api/v2/pokemon/'+pokemonId.toString()+'/');
+ var poke = await response.json();
+ pokemonInfo.push(poke);
+}
+
+async function fetchManyPokemon(pokemonCount) {
+ for(var i = 1; i <= pokemonCount; i++){
+   await fetchPokemon(i);
+ };
+}
+
+async function fetchPokemonImage(pokemonInfo) {
+ var imageUrl = pokemonInfo.sprites.front_default;
+ /*var imageResponse = await fetch(imageUrl);
+ var image = await imageResponse.blob();
+ var base64 = await getBase64(image);*/
+ pokePics.push(imageUrl);
+}
+
+async function fetchPokemonImages() {
+ for(var i = 0; i < pokemonInfo.length; i++){
+   await fetchPokemonImage(pokemonInfo[i]);
+ };
+}
+
+async function getPokemon(pokemonCount){
+   //pokemon = [];
+   await fetchManyPokemon(pokemonCount);
+   await fetchPokemonImages();
+}
+
+async function buildDex(pokemonCount){
+ await getPokemon(pokemonCount);
+ console.table(pokemonInfo);
+ console.table(pokePics);
+}
+
+buildDex(10)
