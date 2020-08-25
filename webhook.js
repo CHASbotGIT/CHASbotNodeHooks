1c7c8a1ea8bf76cd8656b37ce90607e48d75e212
CHASbot <chasbotic@gmail.com>
2020-08-25 22:44:24 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -3947,15 +3947,16 @@ async function fetchPokemon(pokemonId) {
       //console.log(poke);
       console.log("ID: ",poke.id);
       console.log("Name: ",strTitleCase(poke.species.name));
+      console.log("Species URL: ",strTitleCase(poke.species.url));
       console.log("Height: ",poke.height);
       console.log("Weight: ",poke.weight);
       console.log("Sprite: ",poke.sprites.front_default);
-      console.log("HP: ",poke.stats[5].base_stat);
-      console.log("Special Attack: ",poke.stats[4].base_stat);
-      console.log("Special Defence: ",poke.stats[3].base_stat);
-      console.log("Attack: ",poke.stats[2].base_stat);
-      console.log("Defence: ",poke.stats[1].base_stat);
       console.log("Speed: ",poke.stats[0].base_stat);
+      console.log("Defence: ",poke.stats[1].base_stat);
+      console.log("Attack: ",poke.stats[2].base_stat);
+      console.log("Special Defence: ",poke.stats[3].base_stat);
+      console.log("Special Attack: ",poke.stats[4].base_stat);
+      console.log("HP: ",poke.stats[5].base_stat);
       let types = poke.types.map((type) => type.type.name).join(', ').toUpperCase();
       types = types.replace(/,([^,]*)$/, ' &$1');
       console.log("Type(s): ",types);
@@ -3974,4 +3975,36 @@ async function fetchPokemon(pokemonId) {
   }); // req.on('error'
 }
 
+// ███████▒▒▒
+
 fetchPokemon(1);
+
+/*let evoChain = [];
+let evoData = chain.chain;
+
+do {
+  let numberOfEvolutions = evoData.evolves_to.length;
+
+  evoChain.push({
+    "species_name": evoData .species.name,
+    "min_level": !evoData ? 1 : evoData .min_level,
+    "trigger_name": !evoData ? null : evoData .trigger.name,
+    "item": !evoData ? null : evoData .item
+  });
+
+  if(numberOfEvolutions > 1) {
+    for (let i = 1;i < numberOfEvolutions; i++) {
+      evoChain.push({
+        "species_name": evoData.evolves_to[i].species.name,
+        "min_level": !evoData.evolves_to[i]? 1 : evoData.evolves_to[i].min_level,
+        "trigger_name": !evoData.evolves_to[i]? null : evoData.evolves_to[i].trigger.name,
+        "item": !evoData.evolves_to[i]? null : evoData.evolves_to[i].item
+     });
+    }
+  }
+
+  evoData = evoData.evolves_to[0];
+
+} while (evoData != undefined && evoData.hasOwnProperty('evolves_to'));
+
+return evoChain;*/
