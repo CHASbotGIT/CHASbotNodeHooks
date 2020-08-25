da1549352df9c4ce5e4dce9b89f0c01eb3083304
CHASbot <chasbotic@gmail.com>
2020-08-25 21:32:08 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -3959,7 +3959,7 @@ async function fetchPokemon(pokemonId) {
       let abilities = poke.abilities.map((ability) => ability.ability.name).join(', ').toUpperCase();
       console.log("Abilities: ",abilities);
       let moves = poke.moves.map((move) => move.move.name).join(', ').toUpperCase();
-      moves = moves.replace(/,\s*$/, " &");
+      moves = moves.replace(/,([^,]*)$/, ' and $1');
       console.log("Moves: ",moves);
       //pokemonInfo.push(poke);
     }); // res.on('end'
