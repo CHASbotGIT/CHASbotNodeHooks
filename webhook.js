ef74460b3c6fe97b63ea418f14e216ae8e1bca3e
CHASbot <chasbotic@gmail.com>
2020-08-25 23:22:39 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -3951,33 +3951,12 @@ async function fetchPokemon(pokemonId) {
       let evoData = poke.chain;
       console.log(evoData);
 
-      let evoDataTo = poke.chain.evolves_to
+      let evoDataTo = evoData.evolves_to
       console.log(evoDataTo);
 
-      do {
-        let numberOfEvolutions = evoData.evolves_to.length;
+      let evoDataTo2 = evoDataTo.evolves_to
+      console.log(evoDataTo2);
 
-        evoChain.push({
-          "species_name": evoData .species.name,
-          "min_level": !evoData ? 1 : evoData .min_level,
-          "trigger_name": !evoData ? null : evoData .trigger.name,
-          "item": !evoData ? null : evoData .item
-        });
-
-        console.table(evoChain);
-
-        if(numberOfEvolutions > 1) {
-          for (let i = 1;i < numberOfEvolutions; i++) {
-            evoChain.push({
-              "species_name": evoData.evolves_to[i].species.name,
-              "min_level": !evoData.evolves_to[i]? 1 : evoData.evolves_to[i].min_level,
-              "trigger_name": !evoData.evolves_to[i]? null : evoData.evolves_to[i].trigger.name,
-              "item": !evoData.evolves_to[i]? null : evoData.evolves_to[i].item
-           });
-          }
-        }
-
-        evoData = evoData.evolves_to[0];
 
       } while (evoData != undefined && evoData.hasOwnProperty('evolves_to'));
 
