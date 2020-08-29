122c1f23d07f31c1f0a5380f93c827afa6e2093d
CHASbot <chasbotic@gmail.com>
2020-08-29 21:30:34 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1233,10 +1233,10 @@ CHASbot.post('/webhook', (req, res) => {
             console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ",pokedex);
             fetchPokemon(pokedex); //67
             var base_stats = 'BULBASAUR\n' +
-            '░░░░░░▒▒▒▒▒▒▒▒' + ': HP [45]\n' +
-            '░░░░░░░░░▒▒▒▒▒' + ': Attack [49]\n' +
-            '░░░░░░░▒▒▒▒▒▒▒' + ': Defence [49]\n' +
-            '░░░░▒▒▒▒▒▒▒▒▒▒' + ': HP [65]\n';
+            '██████▒▒▒▒▒▒▒▒' + ': HP [45]\n' +
+            '█████████▒▒▒▒▒' + ': Attack [49]\n' +
+            '███████▒▒▒▒▒▒▒' + ': Defence [49]\n' +
+            '████▒▒▒▒▒▒▒▒▒▒' + ': HP [65]\n';
             postImage(event,'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',true,base_stats);
           };
 
