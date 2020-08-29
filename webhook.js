d6840f9f3b9e0749ef9d72436e28e366bd1e8d29
CHASbot <chasbotic@gmail.com>
2020-08-29 12:45:58 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1230,6 +1230,7 @@ CHASbot.post('/webhook', (req, res) => {
           position_in_analyse_text = analyse_text.search(TRIGGER_DEV) + 1;
           if (position_in_analyse_text > 0) {
             console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ",analyse_text);
+            console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ",typeof analyse_text)
             let pokedex = parseInt(analyse_text,10);
             console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ",pokedex);
             pokedex = 67;
