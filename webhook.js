f8c186bae7ca7df6b223af7d04f938c1a50c1647
CHASbot <chasbotic@gmail.com>
2020-08-30 15:21:08 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -4108,29 +4108,30 @@ async function fetchPokemon(pokemonId) {
                   }; // if (typeof holder
                 }; // for (pokeEvoDetailsLoop
               }; // if (typeof evoDataNest
-            }; // for (nestLoop
 
-            var evoPhraseCnt = evoChainNarrative.length;
-            var super_prefix = ' and '; // switch to or?
-            if (evoPhraseCnt == 0) {
-              console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> no specifics')
-            } else if (evoPhraseCnt == 1) {
-              evoNarrative = evoNarrative + ' ' + evoChainNarrative[0];
-            } else if (evoPhraseCnt == 2) {
-              evoNarrative = evoNarrative + ' ' + evoChainNarrative[0] + super_prefix + evoChainNarrative[1];
-            } else { // >2 e.g. 3.... 0,1,2 (length -1)
-              let narLoop = 1;
-              for (narLoop = 0; narLoop < evoPhraseCnt.length; narLoop++) {
-                if (narLoop == (evoPhraseCnt-1)) {
-                  super_prefix = ' and ';
-                } else if (narLoop == 0) {
-                  super_prefix = ' ';
-                } else {
-                  super_prefix = ', ';
+              var evoPhraseCnt = evoChainNarrative.length;
+              var super_prefix = ' and '; // switch to or?
+              if (evoPhraseCnt == 0) {
+                console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> no specifics')
+              } else if (evoPhraseCnt == 1) {
+                evoNarrative = evoNarrative + ' ' + evoChainNarrative[0];
+              } else if (evoPhraseCnt == 2) {
+                evoNarrative = evoNarrative + ' ' + evoChainNarrative[0] + super_prefix + evoChainNarrative[1];
+              } else { // >2 e.g. 3.... 0,1,2 (length -1)
+                let narLoop = 1;
+                for (narLoop = 0; narLoop < evoPhraseCnt.length; narLoop++) {
+                  if (narLoop == (evoPhraseCnt-1)) {
+                    super_prefix = ' and ';
+                  } else if (narLoop == 0) {
+                    super_prefix = ' ';
+                  } else {
+                    super_prefix = ', ';
+                  };
+                  evoNarrative = evoNarrative + super_prefix + evoChainNarrative[narLoop];
                 };
-                evoNarrative = evoNarrative + super_prefix + evoChainNarrative[narLoop];
               };
-            };
+
+            }; // for (nestLoop
 
             evoChain.push({
               "nest": nest,
