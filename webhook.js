c47c7ff4755485f23be30c7f465069fe58c87935
CHASbot <chasbotic@gmail.com>
2020-09-06 14:25:43 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -4303,8 +4303,9 @@ function apiPOKEMONcb(apiCall,callback) {
               description = flavEntry.flavor_text;
             }; // if
           }; // for (flavLoop
+          description = description.replace([\n\f],' ');
           let eggs = poke.egg_groups.map((element) => strTitleCase(element.name)).join(', ');
-          eggs = eggs.replace(/,([^,]*)$/, ' &$1'); // last comma for and
+          eggs = eggs.replace(/,([^,]*)$/,' &$1'); // last comma for and
           let hatch_after = (poke.hatch_counter+1)*255;
           let habitat = "No Habitat";
           if (poke.habitat != null) { habitat = poke.habitat.name };
